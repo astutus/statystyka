e5afa82ee8c48e2bc5f0d3f1b1762a7a3d5223dc
Olimp Bockowski <obockows@redhat.com>
2020-12-25 00:00:02 +0100

    Implementacja KwartylGorny i KwartylDolny

--- zroznicowanie.go
+++ zroznicowanie.go
@@ -2,6 +2,7 @@ package statystyka
 
 import (
 	"log"
+	"math"
 	"sort"
 )
 
@@ -11,28 +12,42 @@ func Rozstep(wejscie []float64) float64 {
 	return wejscie[len(wejscie) - 1] - wejscie[0]
 }
 
+// MinimumRozstepu zwraca namniejsza wartosc w danych
 func MinimumRozstepu(wejscie []float64) float64 {
 	sort.Float64s(wejscie)
 	return wejscie[0]
 }
-
+// MaksimumRozstepu zwraca najwieksza wartosc w danych
 func MaksimumRoztsepu(wejscie []float64) float64 {
 	sort.Float64s(wejscie)
 	return wejscie[len(wejscie) - 1]
 }
 
+// KwartylSrodkowy zwraca wartosc dzielaca zbior danych na dwie czesci
 func KwartylSrodkowy(wejscie []float64) float64 {
 	return Mediana(wejscie)
 }
 
 func KwartylDolny(wejscie []float64) float64 {
-	if len(wejscie) < 3 {
+	sort.Float64s(wejscie)
+	wielkosc := len(wejscie)
+	if wielkosc < 3 {
 		log.Fatalln("za malo elementow, minimalnie 3")
 	}
+	if wielkosc % 4 == 0 {
+		return SredniaArytmetyczna(wejscie[wielkosc/4:wielkosc/4+2])
+	}
+	return  math.Ceil(float64(len(wejscie) / 4.0))
+
 }
 
 func KwartylGorny(wejscie []float64) float64 {
-	if len(wejscie) < 3 {
+	wielkosc := len(wejscie)
+	if wielkosc < 3 {
 		log.Fatalln("za malo elementow, minimalnie 3")
 	}
+	if 3* wielkosc % 4 == 0 {
+		return SredniaArytmetyczna(wejscie[3* wielkosc / 4: 3*wielkosc / 4 + 2])
+	}
+	return math.Ceil(float64(wejscie[3*wielkosc / 4]))
 }
