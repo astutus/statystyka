f2b2e5691c4905c00168b7f4578fb004fafec8c9
Olimp Bockowski <obockows@redhat.com>
2020-12-24 23:29:20 +0100

    Zroznicowanie danych

--- zroznicowanie.go
+++ zroznicowanie.go
@@ -1,6 +1,9 @@
 package statystyka
 
-import "sort"
+import (
+	"log"
+	"sort"
+)
 
 // Rozstep podaje miare na jakiej przestrzeni rozproszone sa dane
 func Rozstep(wejscie []float64) float64 {
@@ -13,7 +16,23 @@ func MinimumRozstepu(wejscie []float64) float64 {
 	return wejscie[0]
 }
 
-func MaksimumRoztsepu(wejscie[]float64) float64 {
+func MaksimumRoztsepu(wejscie []float64) float64 {
 	sort.Float64s(wejscie)
 	return wejscie[len(wejscie) - 1]
+}
+
+func KwartylSrodkowy(wejscie []float64) float64 {
+	return Mediana(wejscie)
+}
+
+func KwartylDolny(wejscie []float64) float64 {
+	if len(wejscie) < 3 {
+		log.Fatalln("za malo elementow, minimalnie 3")
+	}
+}
+
+func KwartylGorny(wejscie []float64) float64 {
+	if len(wejscie) < 3 {
+		log.Fatalln("za malo elementow, minimalnie 3")
+	}
 }
