6ce26186bf2d52e9d9e3b522779fd4311f52c1e3
Olimp Bockowski <obockows@redhat.com>
2021-01-02 15:45:47 +0100

    Syntax error fixed

--- zroznicowanie.go
+++ zroznicowanie.go
@@ -9,7 +9,7 @@ import (
 // Rozstep podaje miare na jakiej przestrzeni rozproszone sa dane
 func Rozstep(wejscie []float64) float64 {
 	sort.Float64s(wejscie)
-	return wejscie[len(wejscie) - 1] - wejscie[0]
+	return wejscie[len(wejscie)-1] - wejscie[0]
 }
 
 // MinimumRozstepu zwraca namniejsza wartosc w danych
@@ -17,16 +17,18 @@ func MinimumRozstepu(wejscie []float64) float64 {
 	sort.Float64s(wejscie)
 	return wejscie[0]
 }
+
 // MaksimumRozstepu zwraca najwieksza wartosc w danych
 func MaksimumRoztsepu(wejscie []float64) float64 {
 	sort.Float64s(wejscie)
-	return wejscie[len(wejscie) - 1]
+	return wejscie[len(wejscie)-1]
 }
 
 // KwartylSrodkowy zwraca wartosc dzielaca zbior danych na dwie czesci
 func KwartylSrodkowy(wejscie []float64) float64 {
 	return Mediana(wejscie)
 }
+
 //KwartylDolny to pierwszy kwartyl po podziale zbioru na cztery rowne czesci
 func KwartylDolny(wejscie []float64) float64 {
 	sort.Float64s(wejscie)
@@ -34,8 +36,8 @@ func KwartylDolny(wejscie []float64) float64 {
 	if wielkosc < 3 {
 		log.Fatalln("za malo elementow, minimalnie 3")
 	}
-	if wielkosc % 4 == 0 {
-		return SredniaArytmetyczna(wejscie[wielkosc/4-1:wielkosc/4+1])
+	if wielkosc%4 == 0 {
+		return SredniaArytmetyczna(wejscie[wielkosc/4-1 : wielkosc/4+1])
 	}
 	pozycja := int(math.Ceil(float64(len(wejscie) / 4.0)))
 	return wejscie[pozycja]
@@ -48,19 +50,20 @@ func KwartylGorny(wejscie []float64) float64 {
 	if wielkosc < 3 {
 		log.Fatalln("za malo elementow, minimalnie 3")
 	}
-	if 3* wielkosc % 4 == 0 {
-		return SredniaArytmetyczna(wejscie[(3* wielkosc / 4) - 1: (3*wielkosc / 4) + 1 ])
+	if 3*wielkosc%4 == 0 {
+		return SredniaArytmetyczna(wejscie[(3*wielkosc/4)-1 : (3*wielkosc/4)+1])
 	}
-	return math.Ceil(float64(wejscie[3*wielkosc / 4]))
+	return math.Ceil(float64(wejscie[3*wielkosc/4]))
 }
+
 // Percentyl okresla procent (uporzadkowanych) danych, ktore odcina
-func Percentyl(procent int, wejscie []float64 ) []float64 {
+func Percentyl(procent int, wejscie []float64) []float64 {
 	dlugosc := len(wejscie)
 	if dlugosc < 100 {
 		log.Fatalln("zbyt malo danych")
 	}
 	sort.Float64s(wejscie)
-	if (procent * dlugosc) % 100 == 0 {
+	if (procent*dlugosc)%100 == 0 {
 		pozycja := procent * dlugosc / 100
 		var result = make([]float64, procent)
 		wartosc := SredniaArytmetyczna([]float64{wejscie[pozycja-1], wejscie[pozycja]})
@@ -72,20 +75,20 @@ func Percentyl(procent int, wejscie []float64 ) []float64 {
 		result = append(result, wartosc)
 		return result
 	}
-	pozycja := math.Ceil(float64(procent * dlugosc) / 100.0)
+	pozycja := math.Ceil(float64(procent*dlugosc) / 100.0)
 	return wejscie[0:int(pozycja)]
 
 }
 
 // Wariacja okresla zmiennosc danych
-func Wariacja(wejscie []]float64) float64 {
+func Wariacja(wejscie []float64) float64 {
 	var suma float64
 	dlugosc := len(wejscie)
 	srednia := SredniaArytmetyczna(wejscie)
 	for _, v := range wejscie {
 		suma += (v * v)
 	}
-	return suma / float64(dlugosc) - srednia * srednia
+	return suma/float64(dlugosc) - srednia*srednia
 }
 
 // OdchylenieStandardowe pozwala nam mierzyc jak bardzo nasze dane roznia sie przecietnie od sredniej
@@ -96,5 +99,5 @@ func OdchylenieStandardowe(wejscie []float64) float64 {
 
 // Standaryzacja danych to sposob na porownanie kilku zbiorow danych rozniacych sie wartosciami sredniej i odchylenia standardowego
 func Standaryzacja(wartosc, sredniaArytmetyczna, odchylenieStandardowe float64) float64 {
-	return wartosc - sredniaArytmetyczna / odchylenieStandardowe
+	return wartosc - sredniaArytmetyczna/odchylenieStandardowe
 }
