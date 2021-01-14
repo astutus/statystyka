f9236dbe5d1ee17e7c8418d1b6b647c9d9f4b91e
Olimp Bockowski <obockows@redhat.com>
2021-01-14 22:18:16 +0100

    Zbiory i operacje
    Zbiory i operacje: intersection, union, difference dla strings, int, float64

---
+++ zbiory.go
@@ -0,0 +1,48 @@
+package statystyka
+
+type zbiorStringow map[string]bool
+type zbiorFloatow map[float64]bool
+type zbiorIntow map[int]bool
+
+// dla Stringow
+func IntersectionZbiorStringow(a zbiorStringow, b zbiorStringow) zbiorStringow {
+	var wynik = zbiorStringow{}
+	return wynik
+}
+func UnionZbiorStringow(a zbiorStringow, b zbiorStringow) zbiorStringow {
+	var wynik = zbiorStringow{}
+	return wynik
+}
+func DifferenceZbiorStringow(a zbiorStringow, b zbiorStringow) zbiorStringow {
+	var wynik = zbiorStringow{}
+	return wynik
+}
+
+// dla float'ow
+func IntersectionZbiorFloatow(a zbiorFloatow, b zbiorFloatow) zbiorFloatow {
+	var wynik = zbiorFloatow{}
+	return wynik
+}
+func UnionZbiorFloatow(a zbiorStringow, b zbiorStringow) zbiorFloatow {
+	var wynik = zbiorFloatow{}
+	return wynik
+}
+func DifferenceZbiorFloatow(a zbiorFloatow, b zbiorFloatow) zbiorFloatow {
+	var wynik = zbiorFloatow{}
+	return wynik
+}
+
+// dla Int'ow
+func IntersectionZbiorIntow(a zbiorIntow, b zbiorIntow) zbiorIntow {
+	var wynik = zbiorIntow{}
+	return wynik
+}
+func UnionZbiorIntow(a zbiorIntow, b zbiorIntow) zbiorIntow {
+	var wynik = zbiorIntow{}
+	return wynik
+}
+func DifferenceZbiorIntow(a zbiorIntow, b zbiorIntow) zbiorIntow {
+	var wynik = zbiorIntow{}
+	return wynik
+}
+
