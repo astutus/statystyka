82b99b8a320cc05c41df6994c654565920089e32
Olimp Bockowski <obockows@redhat.com>
2021-01-15 18:43:17 +0100

    Implementacja metod dla zbiorow

--- zbiory.go
+++ zbiory.go
@@ -5,44 +5,92 @@ type zbiorFloatow map[float64]bool
 type zbiorIntow map[int]bool
 
 // dla Stringow
-func IntersectionZbiorStringow(a zbiorStringow, b zbiorStringow) zbiorStringow {
+func IntersectionZbiorStringow(a, b zbiorStringow) zbiorStringow {
 	var wynik = zbiorStringow{}
+	for k, _ := range a {
+		if b[k] {
+			wynik[k] = true
+		}
+	}
 	return wynik
 }
-func UnionZbiorStringow(a zbiorStringow, b zbiorStringow) zbiorStringow {
+func UnionZbiorStringow(a, b zbiorStringow) zbiorStringow {
 	var wynik = zbiorStringow{}
+	for k, _ := range a {
+		wynik[k] = true
+	}
+	for k, _ := range b {
+		wynik[k] = true
+	}
 	return wynik
 }
-func DifferenceZbiorStringow(a zbiorStringow, b zbiorStringow) zbiorStringow {
+func DifferenceZbiorStringow(a, b zbiorStringow) zbiorStringow {
 	var wynik = zbiorStringow{}
+	for k, _ := range a {
+		if ! b[k] {
+			wynik[k] = true
+		}
+	}
 	return wynik
 }
 
 // dla float'ow
-func IntersectionZbiorFloatow(a zbiorFloatow, b zbiorFloatow) zbiorFloatow {
+func IntersectionZbiorFloatow(a, b zbiorFloatow) zbiorFloatow {
 	var wynik = zbiorFloatow{}
+	for k, _ := range a {
+		if b[k] {
+			wynik[k] = true
+		}
+	}
 	return wynik
 }
-func UnionZbiorFloatow(a zbiorStringow, b zbiorStringow) zbiorFloatow {
+func UnionZbiorFloatow(a, b zbiorFloatow) zbiorFloatow {
 	var wynik = zbiorFloatow{}
+	for k, _ := range a {
+		wynik[k] = true
+	}
+	for k, _ := range b {
+		wynik[k] = true
+	}
 	return wynik
 }
-func DifferenceZbiorFloatow(a zbiorFloatow, b zbiorFloatow) zbiorFloatow {
+func DifferenceZbiorFloatow(a, b zbiorFloatow) zbiorFloatow {
 	var wynik = zbiorFloatow{}
+	for k, _ := range a {
+		if ! b[k] {
+			wynik[k] = true
+		}
+	}
 	return wynik
 }
 
 // dla Int'ow
-func IntersectionZbiorIntow(a zbiorIntow, b zbiorIntow) zbiorIntow {
+func IntersectionZbiorIntow(a, b zbiorIntow) zbiorIntow {
 	var wynik = zbiorIntow{}
+	for k, _ := range a {
+		if b[k] {
+			wynik[k] = true
+		}
+	}
 	return wynik
 }
-func UnionZbiorIntow(a zbiorIntow, b zbiorIntow) zbiorIntow {
+func UnionZbiorIntow(a, b zbiorIntow) zbiorIntow {
 	var wynik = zbiorIntow{}
+	for k, _ := range a {
+		wynik[k] = true
+	}
+	for k, _ := range b {
+		wynik[k] = true
+	}
 	return wynik
 }
-func DifferenceZbiorIntow(a zbiorIntow, b zbiorIntow) zbiorIntow {
+func DifferenceZbiorIntow(a, b zbiorIntow) zbiorIntow {
 	var wynik = zbiorIntow{}
+	for k, _ := range a {
+		if ! b[k] {
+			wynik[k] = true
+		}
+	}
 	return wynik
 }
 
